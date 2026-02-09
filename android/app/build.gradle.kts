1b5d8ee44dbac9fd278eaaf496b6eb35409b7c46
Caleb Campbell <raida85@att.net>
2026-02-09 14:26:51 -0600

    more preparation for closed testing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,20 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("android/key.properties")
+
+println("DEBUG - rootProject dir: ${rootProject.projectDir}")
+println("DEBUG - File exists: ${keystorePropertiesFile.exists()}")
+if (keystorePropertiesFile.exists()){
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
+    println("DEBUG - Loaded keys: ${keystoreProperties.keys}")
 }
 
 android {
@@ -21,28 +33,35 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.soggywombat.spoonie"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 2
         versionName = "Vision 1Ab"
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
         }
     }
-    dependencies{
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.22")
         coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     }
 }
+
 
 flutter {
     source = "../.."
